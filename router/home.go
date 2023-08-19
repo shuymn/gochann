4a7062f0156d5af59061343681b37d7976febd53
shuymn <mail@shuymn.me>
2023-08-19 09:30:36 +0900

    テンプレートファイルを埋め込む

--- router/home.go
+++ router/home.go
@@ -7,13 +7,14 @@ import (
 	"time"
 )
 
+var homeHTML = template.Must(template.ParseFS(templates, "template/home.html"))
+
 func (h *Handler) HomeHandler(w http.ResponseWriter, r *http.Request) {
 	token, err := r.Cookie("token")
 	// cookie に token がないなら home ページを表示
 	if err != nil {
-		t := template.Must(template.ParseFiles("./template/home.html"))
 		w.Header().Set("Content-Type", "text/html; charset=utf-8")
-		if err := t.Execute(w, nil); err != nil {
+		if err := homeHTML.Execute(w, nil); err != nil {
 			log.Printf("ERROR: exec templating err: %v", err)
 			w.WriteHeader(http.StatusInternalServerError)
 			return
@@ -37,9 +38,8 @@ func (h *Handler) HomeHandler(w http.ResponseWriter, r *http.Request) {
 
 	// cookie の情報が session になかった場合
 	if userID == 0 {
-		t := template.Must(template.ParseFiles("./template/home.html"))
 		w.Header().Set("Content-Type", "text/html; charset=utf-8")
-		if err := t.Execute(w, nil); err != nil {
+		if err := homeHTML.Execute(w, nil); err != nil {
 			log.Printf("ERROR: exec templating err: %v", err)
 			w.WriteHeader(http.StatusInternalServerError)
 			return
--- router/router.go
+++ router/router.go
@@ -1,6 +1,15 @@
 package router
 
-import "database/sql"
+import (
+	"database/sql"
+	"embed"
+)
+
+// NOTE: all:template は template ディレクトリ以下の全てのファイルを埋め込む
+// all なしだと _ から始まるファイルは埋め込まれない
+
+//go:embed all:template
+var templates embed.FS
 
 type Handler struct {
 	db *sql.DB
